1d523ed7577bdc0fa8d8b955dbc192b410ab3571
akshit Pathania <akshitpathania75@gmail.com>
2026-01-25 22:50:35 +0530

    port changed

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hult/",
   server: {
     host: "::",
-    port: 8080,
+    port: 8210,
   },
   plugins: [react(), tailwindcss(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
